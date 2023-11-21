8f5806a152b0409d20116b94acad67dc7e2df0f6
haidahrahim26 <haidah266000@gmail.com>
2023-11-22 00:32:08 +0800

    My Lab2 Layout Customization

---
+++ app/src/main/java/com/example/ict602cdcs2703b_haidah_layoutcustomization/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.ict602cdcs2703b_haidah_layoutcustomization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
